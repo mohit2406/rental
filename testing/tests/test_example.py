32c2f1b11084f7b8576f7e61b741f693fd6a1066
Mohit Singh <mohit.singh@elastic.run>
2024-06-30 13:27:58 +0530

    intial commit

--- testing/tests/test_example.py
+++ testing/tests/test_example.py
@@ -1,6 +1,12 @@
 import pytest
 import pandas as pd
 
+@pytest.fixture(scope="session")
+def load_data():
+    data = pd.read_csv("starter/data/census.csv")
+    return data
+
 def test_preprocess_data():
-    data = pd.read_csv('../data/cleaned_data/clean_data.csv')
+    data = load_data()
+    # data = pd.read_csv('../data/cleaned_data/clean_data.csv')
     assert len(data) > 0
