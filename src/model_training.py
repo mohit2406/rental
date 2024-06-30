cabbf3426f3680a3467b8d673f95fda74175d701
Mohit Singh <mohit.singh@elastic.run>
2024-06-30 13:00:44 +0530

    intial commit

--- src/model_training.py
+++ src/model_training.py
@@ -15,6 +15,7 @@ def main():
 
     X = data.drop(['price'],axis=1)
     y=data['price']
+    #train
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # param_grid = {
